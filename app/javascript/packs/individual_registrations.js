5fc12a7798957927d2a950a6ec9b37a795316875
Daniel Pinheiro <dgl.daniel@outlook.com.br>
2022-06-18 02:41:52 -0300

    Improve UX on individual register form using steps

--- app/javascript/packs/individual_registrations.js
+++ app/javascript/packs/individual_registrations.js
@@ -1,5 +1,3 @@
-console.log('chegou aqui')
-
 document.addEventListener('DOMContentLoaded', () => {
   // Functions to open and close a modal
   function openModal($el) {
@@ -44,3 +42,67 @@ document.addEventListener('DOMContentLoaded', () => {
     }
   });
 });
+
+window.onload = function () {
+// step form
+
+const $prevBtn = document.getElementById("prevBtn");
+const $nextBtn = document.getElementById("nextBtn");
+
+$prevBtn.addEventListener("click", () => nextPrev(-1));
+$nextBtn.addEventListener("click", () => nextPrev(1));
+
+var currentTab = 0; // Current tab is set to be the first tab (0)
+showTab(currentTab); // Display the current tab
+
+function showTab(n) {
+  // This function will display the specified tab of the form ...
+  var x = document.getElementsByClassName("tab");
+  x[n].style.display = "block";
+  // ... and fix the Previous/Next buttons:
+  if (n == 0) {
+    document.getElementById("prevBtn").style.display = "none";
+  } else {
+    document.getElementById("prevBtn").style.display = "inline";
+  }
+  if (n == (x.length - 1)) {
+    document.getElementById("nextBtn").innerHTML = "Submit";
+  } else {
+    document.getElementById("nextBtn").innerHTML = "Next";
+  }
+  // ... and run a function that displays the correct step indicator:
+  fixStepIndicator(n)
+}
+
+function nextPrev(n) {
+  console.log('currentTab', currentTab)
+  // This function will figure out which tab to display
+  var x = document.getElementsByClassName("tab");
+  // Exit the function if any field in the current tab is invalid:
+  if (n == 6) return false;
+  // Hide the current tab:
+  if (currentTab < 6) {
+    x[currentTab].style.display = "none";
+  }
+  // Increase or decrease the current tab by 1:
+  currentTab = currentTab + n;
+  // if you have reached the end of the form... :
+  if (currentTab >= x.length) {
+    //...the form gets submitted:
+    document.getElementById("form-register").submit();
+    return false;
+  }
+  // Otherwise, display the correct tab:
+  showTab(currentTab);
+}
+
+function fixStepIndicator(n) {
+  // This function removes the "active" class of all steps...
+  var i, x = document.getElementsByClassName("step");
+  for (i = 0; i < x.length; i++) {
+    x[i].className = x[i].className.replace(" active", "");
+  }
+  //... and adds the "active" class to the current step:
+  x[n].className += " active";
+}
+};
