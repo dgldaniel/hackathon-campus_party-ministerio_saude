94845b58e9cdd4b4664d340ae4afb139bcfb4bc9
Daniel Pinheiro <dgl.daniel@outlook.com.br>
2022-10-26 14:43:44 -0300

    Validates household registrations fields on js

--- app/javascript/packs/household_registrations.js
+++ app/javascript/packs/household_registrations.js
@@ -12,7 +12,50 @@ document.addEventListener('DOMContentLoaded', () => {
     return { ...acc, [key]: cur };
   }, {});
 
-  console.log(inputs)
+  inputs['stSemNumero'].addEventListener('change', event => {
+    if (event.target.checked) {
+      inputs['numero'].disabled = true;
+    } else {
+      inputs['numero'].disabled = false;
+    }
+  });
+
+  const localizacaoButton = document.getElementsByName("household_registration[localizacao]");
+  const areaRuralButton = document.getElementsByName("household_registration[areaProducaoRural]");
+
+  localizacaoButton.forEach((eachLocalizaoInput) => {
+    eachLocalizaoInput.addEventListener('change', event => {
+      if (event.target.value === '83') {
+        areaRuralButton.forEach(eachAreaRuralInput => {
+          eachAreaRuralInput.disabled = true;
+        })
+      } else {
+        areaRuralButton.forEach(eachAreaRuralInput => {
+          eachAreaRuralInput.disabled = false;
+        })
+      }
+    });
+  });
+
+  const animaisDomicilioOptions = document.getElementsByName("household_registration[animaisNoDomicilio][]");
+
+  inputs['stAnimaisNoDomicilio'].addEventListener('change', event => {
+    if (event.target.checked) {
+      inputs['quantosAnimaisNoDomicilio'].disabled = true;
+
+      animaisDomicilioOptions.forEach(eachInput => {
+        eachInput.disabled = true;
+      })
+    } else {
+      inputs['quantosAnimaisNoDomicilio'].disabled = false;
+
+      animaisDomicilioOptions.forEach(eachInput => {
+        eachInput.disabled = false;
+      })
+    }
+  });
+
+
 
 
 });
