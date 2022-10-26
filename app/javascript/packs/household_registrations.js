a6ee41351cb42741842e92b6ca1dddadd0587250
Daniel Pinheiro <dgl.daniel@outlook.com.br>
2022-10-25 22:45:16 -0300

    Organize household registrations fields

--- app/javascript/packs/household_registrations.js
+++ app/javascript/packs/household_registrations.js
@@ -1,1 +1,18 @@
 import "cocoon-js-vanilla";
+
+document.addEventListener('DOMContentLoaded', () => {
+  const currentInputs = Array.from(document.querySelectorAll('input[id^="household_registration"]'));
+
+  const inputs = currentInputs.reduce((acc, cur) => {
+    if (cur.id.includes('attributes')) return { ...acc };
+
+    const splitData = cur.id.split('_');
+    const key = splitData[splitData.length - 1]
+
+    return { ...acc, [key]: cur };
+  }, {});
+
+  console.log(inputs)
+
+
+});
