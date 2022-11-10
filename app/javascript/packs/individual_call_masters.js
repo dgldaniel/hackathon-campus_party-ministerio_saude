5dca709d4bacb4c634dd360d40f5fb84c3a4fc96
Daniel Pinheiro <dgl.daniel@outlook.com.br>
2022-11-10 02:17:17 -0300

    Validate js fields on individual call

--- app/javascript/packs/individual_call_masters.js
+++ app/javascript/packs/individual_call_masters.js
@@ -1,19 +1,43 @@
-$(document).on('ready page:load turbolinks:load', function() {
-  $('#individual_calls').on('cocoon:after-insert', function(e, insertedItem, originalEvent) {
-    console.log('element', e)
+import 'jquery-validation';
+const { validateCns } = require('validateCns');
 
-    alert('inseriu')
+$(document).on('ready page:load turbolinks:load', function() {
+  $('#individual_calls').on('cocoon:after-insert', function(e) {
+    require('datepicker/date-picker/datepicker')
+    require('datepicker/date-picker/datepicker.pt-BR')
+
+    const cnsInputs = $('input[id$="cns"]');
+
+    cnsInputs.each((index, item) => {
+      item.addEventListener('blur', (event) => validateCns(event, item))
+    })
+
+    const sexoInput = $('input[name$="[sexo]"]');
+
+    sexoInput.each((index, item) => {
+      item.addEventListener('change', (event) => {
+        const radioButtonValue = event.target.value;
+
+        const [number] = event.target.id.match(/(\d+)/)
+
+        const idadeGestacionalInput = $(`input[id$="${number}_idadeGestacional"]`);
+        const stGravidezPlanejadaInput = $(`input[id$="${number}_stGravidezPlanejada"]`);
+        const nuGestasPreviasInput = $(`input[id$="${number}_nuGestasPrevias"]`);
+        const nuPartosInput = $(`input[id$="${number}_nuPartos"]`);
+
+        if (radioButtonValue === '0') {
+          idadeGestacionalInput.attr('value', '')
+          stGravidezPlanejadaInput.attr('value', '')
+          nuGestasPreviasInput.attr('value', '')
+          nuPartosInput.attr('value', '')
+        }
+
+        idadeGestacionalInput.attr('disabled', radioButtonValue === '0')
+        stGravidezPlanejadaInput.attr('disabled', radioButtonValue === '0')
+        nuGestasPreviasInput.attr('disabled', radioButtonValue === '0')
+        nuPartosInput.attr('disabled', radioButtonValue === '0')
+        }
+      )
+    })
   });
 });
-
-// $(function(){
-//   $('#individual_calls').on('cocoon:before-insert', function(e, insertedItem, originalEvent) {
-//     console.log(e, insertedItem, originalEvent)
-
-//     alert('inseriu')
-//   });
-
-//  $("a.add_fields").on('click', function(){
-//    console.log('looks like clicking works...');
-//  });
-// });
